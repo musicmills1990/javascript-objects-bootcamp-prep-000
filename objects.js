5ddee69348dc23249d21083c8974bd4e95009db7
mdmills1990@gmail.com <mdmills1990@gmail.com>
2018-07-11 05:24:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,4 +10,13 @@ function updatePlaylist(playlist,artistName,songTitle){
 function removeFromPlaylist(playlist,artistName){
   delete playlist[artistName];
   return playlist;
+  ['Slowdive']:'Alison',
+  ['My Bloody Valentine']:'Sometimes'}
+  
+function updatePlaylist(playlist,artistName,songTitle){
+  return Object.assign({playlist},{'artistName':'songTitle'})
+}
+function removeFromPlaylist(playlist,artistName){
+  return delete playlist.artistName;
+  return playlist;
 }
